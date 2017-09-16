bbae8378aeb4bca36267786dc3843e9cefe8c47a
Tony Yang <tyang27@ugradx.cs.jhu.edu>
2017-09-16 14:24:31 -0400

    added MIDI notes

--- PhotoToMusic/app/src/main/java/com/example/tony_laptop/phototomusic/MainActivity.java
+++ PhotoToMusic/app/src/main/java/com/example/tony_laptop/phototomusic/MainActivity.java
@@ -13,16 +13,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    /*
     public void addListenerOnCamButton() {
         Button button = (Button) findViewById(R.id.button2);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
+                final int REQUEST_IMAGE_CAPTURE = 1;
                 Intent camIntent = new Intent("android.media.action.IMAGE_CAPTURE");
+                if (camIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(camIntent, REQUEST_IMAGE_CAPTURE);
+
+                }
             }
+
+            void onActivityResult(int requestCode, int resultCode, Intent data) {
+                if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+                    Bundle extras = data.getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    mImageView.setImageBitmap(imageBitmap);
+                }
+            }
+
         });
 
+
+    }*/
+
+    public void addListenerBtnRandom() {
+        Button button = (Button) findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent processImage = new Intent();
+            }
+
+        }
     }
 }
