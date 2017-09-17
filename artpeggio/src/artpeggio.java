f04697f389230dc040ee55e7ed72cca35f75a895
Mason Cole <mason.s.cole@hotmail.com>
2017-09-17 03:11:38 -0400

    Adding a browse for image, simplifying analysis method

--- artpeggio/src/artpeggio.java
+++ artpeggio/src/artpeggio.java
@@ -14,6 +14,9 @@ import com.drew.metadata.exif.*;*/
 
 public class artpeggio {
    private static gui ArtInterface;
+   public static String imgPath = " ";
+   public static int numSamples =20;
+   static int[][] rgb = new int[(numSamples + 1) * (numSamples + 1)][3];;
   // public static Thread InterfaceThread;
 
       public static void main(String[] args) {
@@ -33,35 +36,32 @@ public class artpeggio {
       AudioPermission permission = new AudioPermission("permission", "play");
       String filepath = "artpeggio/resources/drawables/ex02.jpg";
 
-      int numSamples = 20;
-      int[][] rgb = new int[(numSamples + 1) * (numSamples + 1)][3];
-      int key = loadPhoto(filepath, rgb, numSamples);
-      playMusic(rgb, key);
+
+      loadPhoto(1);
    }
 
-   public static int loadPhoto(String filepath, int[][] rgb, int numSamples) {
+   public static void loadPhoto(int DemoNum) {
       BufferedImage image = null;
-      File f = null;
-
+      switch (DemoNum) {
+         case 1: imgPath="ex01.jpg";
+               break;
+         case 2: imgPath="ex02.jpg";
+               break;
+         case 3: imgPath="ex03.jpg";
+               break;
+      }
       try {
-         f = new File(filepath);
-         image = ImageIO.read(f);
+         image = ImageIO.read(artpeggio.class.getResourceAsStream("drawables/"+imgPath));
+      } catch (IOException e) {
+         e.printStackTrace();
       }
-      catch (IOException e) {
-         System.out.println(e);
-      }
-      catch (Exception e) {
-         System.out.println(e);
-      }
-
-
       //array for pixels
-try {
-   byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-}catch (Exception e)
-{
-      e.printStackTrace();
-}
+      try {
+         byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+      }catch (Exception e)
+      {
+         e.printStackTrace();
+      }
       //image width and height
       int width = image.getWidth();
       int height = image.getHeight();
@@ -96,7 +96,77 @@ try {
       rgbTot[1] = rgbTot[1]/pixelsSampled;
       rgbTot[2] = rgbTot[2]/pixelsSampled;
       int total_mean = (rgbTot[0] + rgbTot[1] + rgbTot[2]) / 3;
-      return total_mean;
+      int key = total_mean;
+
+      playMusic(rgb,key);
+   }
+
+
+
+
+
+
+
+   public static void loadPhoto(String filepath) {
+      BufferedImage image = null;
+      File f = null;
+
+      try {
+         f = new File(filepath);
+         image = ImageIO.read(f);
+      }
+      catch (IOException e) {
+         System.out.println(e);
+      }
+      catch (Exception e) {
+         System.out.println(e);
+      }
+
+
+      //array for pixels
+      try {
+         byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+      }catch (Exception e)
+      {
+         e.printStackTrace();
+      }
+      //image width and height
+      int width = image.getWidth();
+      int height = image.getHeight();
+      //rgbTot calculates total, in order to do mean calculation later
+      int[] rgbTot = new int[3];
+      //volume
+      int vol = 100;
+      //total number of pixels grabbed so far
+      int pixelsSampled = 0;
+
+      for (int y = 0; y < height; y += Math.ceil(height/numSamples)) { //y++) {
+         for (int x = 0; x < width; x += Math.ceil(width/numSamples)) { //x++) {
+            int clr = image.getRGB(x,y);
+            rgbTot[0] += (int) ((clr & 0x00ff0000) >> 16);
+            rgbTot[1] += (int) ((clr & 0x0000ff00) >> 8);
+            rgbTot[2] += (int) clr & 0x000000ff;
+
+            rgb[pixelsSampled][1] = ((clr & 0x0000ff00) >> 8);
+            rgb[pixelsSampled][2]= (clr & 0x000000ff);
+
+            System.out.printf("Loop: %d Rt: %5d Gt: %5d Bt: %5d | ", pixelsSampled,
+               (rgbTot[0]), (rgbTot[1]), (rgbTot[2]));
+            System.out.printf("Rv: %3d Gv: %3d Bv: %3d\n",
+               (rgb[pixelsSampled][0]/2), (rgb[pixelsSampled][1]/2),
+               (rgb[pixelsSampled][2]/2));
+            pixelsSampled++;
+         }
+
+      }
+      assert pixelsSampled != 0;
+      rgbTot[0] = rgbTot[0]/pixelsSampled;
+      rgbTot[1] = rgbTot[1]/pixelsSampled;
+      rgbTot[2] = rgbTot[2]/pixelsSampled;
+      int total_mean = (rgbTot[0] + rgbTot[1] + rgbTot[2]) / 3;
+      int key = total_mean;
+
+      playMusic(rgb,key);
    }
 
    public static void playMusic(int rgb[][], int key) {
--- artpeggio/src/gui.java
+++ artpeggio/src/gui.java
@@ -1,21 +1,21 @@
-import sun.applet.Main;
-
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.BufferedInputStream;
-import java.io.File;
 import java.io.IOException;
-import java.io.FileInputStream;
 import java.awt.Image;
 
 import java.io.InputStream;
-import java.net.URL;
+
 public class gui extends JFrame {
    BufferedImage[] images;
+   BufferedImage logoImage=null;
    JPanel LoadPanel;
+   JLabel logoLabel;
    JLabel loadLabel;
    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
    final double FrameWidthMulti = screenSize.getWidth() / 1920;
@@ -25,35 +25,76 @@ public class gui extends JFrame {
    Image LoadIMG;
    ImageIcon loadImg;
    JPanel Options;
-   private boolean isBuffered=false;
+   private boolean isBuffered = false;
+   JTextField filePath;
+   Dimension windowSize;
+   InputStream sourceimage;
+   Image bgImage;
+   JButton browse;
+   boolean LogoisBuffered;
+   JFileChooser imageSelector;
 
-   public gui()
-   {
+
+
+   public gui() {
       super("artPeggio");
       images = new BufferedImage[64];
-      setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-      Dimension windowSize = new Dimension(FrameWidth, FrameHeight);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      windowSize= new Dimension(FrameWidth, FrameHeight);
       this.setSize(windowSize);
       this.setPreferredSize(windowSize);
       this.setResizable(false);
-      Options = new JPanel();
-      LoadPanel=new JPanel();
-      Options.setSize(windowSize);
+      LoadPanel = new JPanel();
       this.setVisible(false);
-      loadLabel=new JLabel();
+      loadLabel = new JLabel();
       loadLabel.setLayout(new BorderLayout());
-      loadLabel.setVisible(true);
-      this.add(loadLabel);
+      loadLabel.setVisible(false);
       LoadPanel.setSize(windowSize);
-      
+      Options = new JPanel();
+      this.setLayout(new GridLayout(2,1));
       setlook();
-      BufferLoading();
+
+
+      loadOptions();
+     // BufferLoading();
+
    }
-   public void initOptions(){
-      this.removeAll();
+
+   public void browseBtn(){
+      browse = new JButton("Browse Image");
+      browse.setPreferredSize(new Dimension((int)(320*FrameWidthMulti), (int)(180*FrameHeightMulti)));
+      browse.setSize((int)(320*FrameWidthMulti), (int)(180*FrameHeightMulti));
+
+      browse.setBorder(BorderFactory.createSoftBevelBorder(2, Color.blue,Color.black));
+      Options.add(browse,BorderLayout.NORTH);
+            browse.addActionListener(new ActionListener() {
+               @Override
+               public void actionPerformed(ActionEvent e) {
+                  browse.setText("browsing...");
+                  imageSelector = new JFileChooser();
+                  imageSelector.setFileSelectionMode(JFileChooser.FILES_ONLY);//only allows selection of files
+                  int selectV = imageSelector.showOpenDialog(null);
+                  if (selectV == JFileChooser.APPROVE_OPTION) {
+                     filePath.setText(imageSelector.getSelectedFile().toString());
+                  }
+            }
+   });}
+
+   public void initOptions() {
+      Options.setBackground(Color.darkGray);
+      Options.setSize(windowSize);
+      Options.setLayout(new FlowLayout(1,2,10));//Flow Layout
+      Options.setBackground(Color.darkGray);
+      browseBtn();
       this.add(Options);
+      Options.setOpaque(false);
+      Options.setVisible(true);
+      this.setVisible(true);
       this.pack();
+
    }
+
+
    public void setlook() {
       try {
          UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -67,7 +108,11 @@ public class gui extends JFrame {
          e.printStackTrace();
       }
    }
-   private BufferedImage getScaledImage(Image srcImg, int w, int h){
+   public void loadOptions(){
+      logoImageLoad();
+      initOptions();
+   }
+   private BufferedImage getScaledImage(Image srcImg, int w, int h) {
       BufferedImage resized = new BufferedImage(w, h, BufferedImage.TRANSLUCENT);
       Graphics2D g2 = resized.createGraphics();
       g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
@@ -75,36 +120,79 @@ public class gui extends JFrame {
       g2.dispose();
       return resized;
    }
-   public void BufferLoading()
-   {
-      new Thread(){
-          public void run(){
-        InputStream sourceimage = new InputStream() {
-         @Override
-         public int read() throws IOException {
-            return 0;
-         }
-            };
-          for (int i = 0; i < 64; i++) {
-             try {
-            if (i <= 9) {
-               sourceimage = this.getClass().getResourceAsStream("intro/Intro0" + i + ".jpg");
-            } else if (i > 9) {
-               sourceimage = this.getClass().getResourceAsStream("intro/Intro" + i + ".jpg");
-            }
 
-            images[i] = getScaledImage(ImageIO.read(new BufferedInputStream(sourceimage)), FrameWidth, FrameHeight);
-         } catch (IOException e) {
-            e.printStackTrace();
-         }
-      } isBuffered=true;
+
+   public void runSelection(int demo){
+      artpeggio.loadPhoto(demo);
+   }
+
+
+   public void logoImageLoad() {
+      logoLabel = new JLabel();
+      /*InputStream  sourceimages;
+       sourceimages = this.getClass().getResourceAsStream("intro/logo.jpg");
+
       try {
-         sourceimage.close();
+         logoImage = getScaledImage(ImageIO.read(new BufferedInputStream(sourceimage)), FrameWidth, FrameHeight);
       } catch (IOException e) {
          e.printStackTrace();
-      }}
-   }.run();}
-public void initLoad(){
+      }
+
+
+      try {
+         sourceimages.close();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+         try{
+         logoLabel.setIcon(new ImageIcon(logoImage));
+         this.add(logoLabel, BorderLayout.NORTH);
+            logoLabel.setVisible(true);
+            logoLabel.setOpaque(true);
+            this.pack();
+         }
+         catch(NullPointerException e)
+      {   e.printStackTrace();
+      }
+      */
+      logoLabel.setIcon(new ImageIcon(getClass().getResource("intro/logo.png")));
+      this.add(logoLabel, BorderLayout.NORTH);
+
+
+   }
+   public void BufferLoading() {
+      new Thread() {
+                  public void run() {
+                     sourceimage = new InputStream() {
+                        @Override
+                        public int read() throws IOException {
+                           return 0;
+                        }
+            };
+            for (int i = 0; i < 64; i++) {
+               try {
+                  if (i <= 9) {
+                     sourceimage = this.getClass().getResourceAsStream("intro/Intro0" + i + ".jpg");
+                  } else if (i > 9) {
+                     sourceimage = this.getClass().getResourceAsStream("intro/Intro" + i + ".jpg");
+                  }
+
+                  images[i] = getScaledImage(ImageIO.read(new BufferedInputStream(sourceimage)), FrameWidth, FrameHeight);
+               } catch (IOException e) {
+                  e.printStackTrace();
+               }
+            }
+            isBuffered = true;
+            try {
+               sourceimage.close();
+            } catch (IOException e) {
+               e.printStackTrace();
+            }
+         }
+      }.run();
+   }
+
+   public void initLoad() {
       this.setVisible(true);
       this.removeAll();
       this.add(loadLabel);
@@ -112,7 +200,7 @@ public void initLoad(){
       {
          //wait for images to be buffered
       }
-      for (int i=0; i<64; i++) {
+      for (int i = 0; i < 64; i++) {
          loadLabel.setIcon(new ImageIcon(images[i]));
          this.pack();
 
@@ -125,7 +213,6 @@ public void initLoad(){
       }
 
    }
-
 
 
 }
