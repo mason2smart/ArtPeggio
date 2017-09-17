8a7030bde5e1a1a08e3b1b16ee1311a7fbc5dd6a
Mason Cole <mason.s.cole@hotmail.com>
2017-09-16 21:19:58 -0400

    changes

--- artpeggio/src/gui.java
+++ artpeggio/src/gui.java
@@ -1,8 +1,33 @@
 import javax.swing.JFrame;
-
+import javax.swing.*;
+import java.awt.*;
 public class gui extends JFrame {
-
-
+   final double FrameSizeMulti = Screensize.
+final int FrameWidth=800;
+final int FrameHeight=1000*;
+   public gui()
+   {
+      JPanel LoadPanel;
+      Dimension windowSize = new Dimension(FrameWidth, FrameHeight);
+      this.setSize(windowSize);
+      this.ins
+      setlook();
+}
+   public void setlook() {
+      try {
+         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+      } catch (ClassNotFoundException e) {
+         e.printStackTrace();
+      } catch (InstantiationException e) {
+         e.printStackTrace();
+      } catch (IllegalAccessException e) {
+         e.printStackTrace();
+      } catch (UnsupportedLookAndFeelException e) {
+         e.printStackTrace();
+      }
+   }
 
 
 }
+
+
