dfbe656ba4cdabf819b1f8dec7c700b364ecb7ec
Mason Cole <mason.s.cole@hotmail.com>
2017-09-17 00:47:22 -0400

    Switching to resource stream from file.io
    Cant import files in jars with file read method needs to be a resource
    stream

--- artpeggio/src/gui.java
+++ artpeggio/src/gui.java
@@ -4,11 +4,14 @@ import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.FileInputStream;
 import java.awt.Image;
 
+import java.io.InputStream;
 import java.net.URL;
 public class gui extends JFrame {
    JPanel LoadPanel;
@@ -16,16 +19,29 @@ public class gui extends JFrame {
    Dimension screenSize= Toolkit.getDefaultToolkit().getScreenSize();
    final double FrameWidthMulti = screenSize.getWidth()/1920;
    final double FrameHeightMulti = screenSize.getHeight()/1080;
-   final int FrameWidth=(int)(800*FrameWidthMulti);
-   final int FrameHeight=(int)(1000*FrameHeightMulti);
+   final int FrameWidth=(int)(400*FrameWidthMulti);
+   final int FrameHeight=(int)(800*FrameHeightMulti);
    Image LoadIMG;
+   ImageIcon loadImg;
+   BufferedImage buffy;
+
    public gui()
    {
       super("artPeggio");
       setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
       Dimension windowSize = new Dimension(FrameWidth, FrameHeight);
       this.setSize(windowSize);
+      this.setPreferredSize(windowSize);
       this.setResizable(false);
+      LoadPanel=new JPanel();
+      this.setVisible(false);
+      loadLabel=new JLabel();
+      loadLabel.setLayout(new BorderLayout());
+      loadLabel.setVisible(true);
+      this.add(loadLabel);
+      LoadPanel.setSize(windowSize);
+
+
       setlook();
       initLoading();
 }
@@ -42,43 +58,53 @@ public class gui extends JFrame {
          e.printStackTrace();
       }
    }
+   private BufferedImage getScaledImage(Image srcImg, int w, int h){
+      BufferedImage resized = new BufferedImage(w, h, BufferedImage.TRANSLUCENT);
+      Graphics2D g2 = resized.createGraphics();
+      g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+      g2.drawImage(srcImg, 0, 0, w, h, null);
+      g2.dispose();
+      return resized;
+   }
    public void initLoading()
    {
-      LoadPanel=new JPanel();
-      Image loadIMG = null;
-      ImageIcon loadImg;
-      loadLabel=new JLabel();
-      loadLabel.setLayout(new BorderLayout());
-      loadLabel.setVisible(true);
-      LoadPanel.add(loadLabel);
-      File sourceimage=new File("artpeggio/resources/drawables/ex02.jpg");
-      for (int i=0; i<=64; i++) {
-         if (i < 9){
-
-            sourceimage=new File("artpeggio/resources/IntroJPGSequence/Intro0"+i+".jpg" );
+      InputStream sourceimage=new InputStream() {
+         @Override
+         public int read() throws IOException {
+            return 0;
          }
-         else {
-          sourceimage=new File("artpeggio/resources/IntroJPGSequence/Intro"+i+".jpg" );
-         }
-      try {
-         loadIMG=ImageIO.read(sourceimage);
-         loadImg = new ImageIcon(loadIMG.getScaledInstance(FrameWidth, FrameHeight, Image.SCALE_SMOOTH));
-         loadLabel.setIcon(loadImg);
-         this.pack();
-         }
-      catch (Exception e)
-      {
-         e.printStackTrace();
-      }
+      };
+      BufferedImage[] images = new BufferedImage[64];
+      for (int i=0; i<64; i++) {
          try {
-            Thread.sleep(200);
+            if (i <= 9) {
+               sourceimage = this.getClass().getResourceAsStream("intro/Intro0" + i + ".jpg");
+            } else if (i > 9) {
+               sourceimage = this.getClass().getResourceAsStream("intro/Intro" + i + ".jpg");
+            if (i==60)
+               this.setVisible(true);
+            }
+
+            images[i] = getScaledImage(ImageIO.read(new BufferedInputStream(sourceimage)), FrameWidth, FrameHeight);
+         } catch (IOException e) {
+            e.printStackTrace();
+         }
+      }
+
+
+
+      //try {
+      for (int i=0; i<64; i++) {
+         loadLabel.setIcon(new ImageIcon(images[i]));
+         this.pack();
+
+         try {
+            Thread.sleep(84);
          } catch (InterruptedException e) {
             e.printStackTrace();
          }
 
       }
-      this.pack();
-      this.setVisible(true);
 
    }
 
