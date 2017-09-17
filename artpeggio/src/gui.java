317a1ea2332bf7ee6fc14422369995900bbbaad1
Mason Cole <mason.s.cole@hotmail.com>
2017-09-17 01:05:20 -0400

    modularizing code

--- artpeggio/src/gui.java
+++ artpeggio/src/gui.java
@@ -14,37 +14,46 @@ import java.awt.Image;
 import java.io.InputStream;
 import java.net.URL;
 public class gui extends JFrame {
+   BufferedImage[] images;
    JPanel LoadPanel;
    JLabel loadLabel;
-   Dimension screenSize= Toolkit.getDefaultToolkit().getScreenSize();
-   final double FrameWidthMulti = screenSize.getWidth()/1920;
-   final double FrameHeightMulti = screenSize.getHeight()/1080;
-   final int FrameWidth=(int)(400*FrameWidthMulti);
-   final int FrameHeight=(int)(800*FrameHeightMulti);
+   Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+   final double FrameWidthMulti = screenSize.getWidth() / 1920;
+   final double FrameHeightMulti = screenSize.getHeight() / 1080;
+   final int FrameWidth = (int) (400 * FrameWidthMulti);
+   final int FrameHeight = (int) (800 * FrameHeightMulti);
    Image LoadIMG;
    ImageIcon loadImg;
-   BufferedImage buffy;
+   JPanel Options;
+   private boolean isBuffered=false;
 
    public gui()
    {
       super("artPeggio");
+      images = new BufferedImage[64];
       setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
       Dimension windowSize = new Dimension(FrameWidth, FrameHeight);
       this.setSize(windowSize);
       this.setPreferredSize(windowSize);
       this.setResizable(false);
+      Options = new JPanel();
       LoadPanel=new JPanel();
+      Options.setSize(windowSize);
       this.setVisible(false);
       loadLabel=new JLabel();
       loadLabel.setLayout(new BorderLayout());
       loadLabel.setVisible(true);
       this.add(loadLabel);
       LoadPanel.setSize(windowSize);
-
-
+      
       setlook();
-      initLoading();
-}
+      BufferLoading();
+   }
+   public void initOptions(){
+      this.removeAll();
+      this.add(Options);
+      this.pack();
+   }
    public void setlook() {
       try {
          UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -66,34 +75,43 @@ public class gui extends JFrame {
       g2.dispose();
       return resized;
    }
-   public void initLoading()
+   public void BufferLoading()
    {
-      InputStream sourceimage=new InputStream() {
+      new Thread(){
+          public void run(){
+        InputStream sourceimage = new InputStream() {
          @Override
          public int read() throws IOException {
             return 0;
          }
-      };
-      BufferedImage[] images = new BufferedImage[64];
-      for (int i=0; i<64; i++) {
-         try {
+            };
+          for (int i = 0; i < 64; i++) {
+             try {
             if (i <= 9) {
                sourceimage = this.getClass().getResourceAsStream("intro/Intro0" + i + ".jpg");
             } else if (i > 9) {
                sourceimage = this.getClass().getResourceAsStream("intro/Intro" + i + ".jpg");
-            if (i==60)
-               this.setVisible(true);
             }
 
             images[i] = getScaledImage(ImageIO.read(new BufferedInputStream(sourceimage)), FrameWidth, FrameHeight);
          } catch (IOException e) {
             e.printStackTrace();
          }
+      } isBuffered=true;
+      try {
+         sourceimage.close();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }}
+   }.run();}
+public void initLoad(){
+      this.setVisible(true);
+      this.removeAll();
+      this.add(loadLabel);
+      while (!isBuffered)//ensures the images are already buffered and stored in array ~10 second process.
+      {
+         //wait for images to be buffered
       }
-
-
-
-      //try {
       for (int i=0; i<64; i++) {
          loadLabel.setIcon(new ImageIcon(images[i]));
          this.pack();
